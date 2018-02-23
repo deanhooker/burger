bd16b461d513ceb9c24d03016c91e44f28de4624
Dean Hooker <deanhooker91@gmail.com>
2018-02-22 18:26:56 -0700

    mysql connection created

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,17 @@
-// Inside the connection.js file, setup the code to connect Node to MySQL.
-// Export the connection.
+// Create mysql connection
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
+
+module.exports = connection;
