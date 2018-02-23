3afc2edcb9842f3edf53b30c23d7cdf229eef0e5
Dean Hooker <deanhooker91@gmail.com>
2018-02-22 18:12:53 -0700

    sql database setup

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,13 @@
--- Create the burgers_db.
--- Switch to or use the burgers_db.
--- Create a burgers table with these fields:
+-- Create database and table
+DROP DATABASE IF EXISTS burgers_db;
 
+CREATE DATABASE burgers_db;
 
+USE burgers_db;
 
--- id: an auto incrementing int that serves as the primary key.
-
--- burger_name: a string.
-
--- devoured: a boolean.
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,14 @@
--- Still in the db folder, create a seeds.sql file. In this file, write insert queries to populate the burgers table with at least three entries.
+-- Populate database with some intial values
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Sweet Home Avocado Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Sympathy for the Deviled Egg Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("I Fought the Slaw Burger (And the Slaw Won)");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Poblano Picasso Burger");
