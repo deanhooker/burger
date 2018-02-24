123baac005512616aed40cba8a7719b6b534321b
Dean Hooker <deanhooker91@gmail.com>
2018-02-24 14:35:38 -0700

    burger.js model commit

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,24 @@
-// nside burger.js, import orm.js into burger.js
+// Import orm.js
+var orm = require("../config/orm.js");
 
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-// Export at the end of the burger.js file.
+// Call ORM functions in burger variable
+var burger = {
+    all: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(set, condition, cb) {
+      orm.updateOne("burgers", set, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
