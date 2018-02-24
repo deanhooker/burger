515d17205f29418488d7a6b8771783d306ad36da
Dean Hooker <deanhooker91@gmail.com>
2018-02-24 14:45:48 -0700

    controller.js commit

--- models/burger.js
+++ models/burger.js
@@ -3,22 +3,22 @@ var orm = require("../config/orm.js");
 
 // Call ORM functions in burger variable
 var burger = {
-    all: function(cb) {
-      orm.selectAll("burgers", function(res) {
-        cb(res);
-      });
+    all: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
     },
     // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.insertOne("burgers", cols, vals, function(res) {
-        cb(res);
-      });
+    create: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
     },
-    update: function(set, condition, cb) {
-      orm.updateOne("burgers", set, condition, function(res) {
-        cb(res);
-      });
+    update: function (set, condition, cb) {
+        orm.updateOne("burgers", set, condition, function (res) {
+            cb(res);
+        });
     }
-  };
-  // Export the database functions for the controller (catsController.js).
-  module.exports = burger;
+};
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
